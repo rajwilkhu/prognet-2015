e4794942bcf28f5e7ed54ee0814df4f416b88b5a
rajpal.wilkhu <rajpal.wilkhu@just-eat.com>
2015-07-01 18:54:39 +0100

    Added key pair generation

--- src/ex1-windows-ec2/README.md
+++ src/ex1-windows-ec2/README.md
@@ -5,5 +5,12 @@
 ## Creating a security group allowing WinRM access to the remote instance
 
 ```powershell
-.\create-ec2-securitygroup -name <come up with a name>
+.\create-ec2-securitygroup.ps1 -name <your security group name>
 ```
+
+## Creating a key pair for accessing the password on the new instance
+
+```powershell
+.\create-ec2-keypair.ps1 -name <your key pair name>
+
+This script will generate an ec2 key pair and write a new pem file locally 
---
+++ src/ex1-windows-ec2/create-ec2-keypair.ps1
@@ -0,0 +1,23 @@
+param([Parameter(Mandatory=$true)][string]$name)
+
+function New-KeyPair
+{
+    param([string]$name = 'prognet',
+          [string]$region = 'eu-west-1')
+    Set-DefaultAWSRegion $region
+
+    $folder = resolve-path .
+    $keyfile = Join-Path -Path $folder -ChildPath "$name.pem"
+    if (Test-Path $keyfile -PathType Leaf)
+    {
+        throw "New-KeyPair - $keyfile already exists"
+    }
+
+    $keypair = New-EC2KeyPair -KeyName $name -ProfileName 'prognet'
+
+    "$($keypair.KeyMaterial)" | Out-File -Encoding ascii -Filepath $keyfile
+    "KeyName: $($keypair.KeyName)" | Out-File -encoding ascii -Filepath $keyfile -Append
+    "KeyFingerprint: $($keypair.KeyFingerprint)" | Out-File -Encoding ascii -Filepath $keyfile -Append
+}
+
+New-KeyPair -name $name
